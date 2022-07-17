74dfe127adccf059b49ef0423ec9ac3e41c1df6f
IvanFestina <ivanfestina@gmail.com>
2022-07-18 01:22:18 +0300

    first commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import {SafeAreaView, Text, StyleSheet} from "react-native";
+
+
+export const HomeScreen = () => {
+
+    return (
+        <SafeAreaView>
+            <Text>I am the homescreen</Text>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({})
